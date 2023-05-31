6afcd09412e13a1518c12d8c93b7fe32060c6212
shubham bhati <112773220+Shubh2-0@users.noreply.github.com>
2023-06-01 02:31:30 +0530

    update project

--- Example2/src/com/masai/Demo.java
+++ Example2/src/com/masai/Demo.java
@@ -1,8 +1,16 @@
 package com.masai;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Demo {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 	
 	String resource1 = "Water";
 	String resource2 = "Spices";
@@ -12,6 +20,15 @@ public class Demo {
 	synchronized (resource1) {
 	
 	System.out.println(Thread.currentThread().getName()+" have "+resource1);
+	
+	try {
+		
+		resource1.wait(1000);
+		
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 	
 	synchronized (resource2) {
 
@@ -31,9 +48,19 @@ public class Demo {
 		
 	System.out.println(Thread.currentThread().getName()+" have "+resource2);
 	
+	
+	try {
+		resource2.wait(1000);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated cathch block
+		e.printStackTrace();
+	}
+	
 	synchronized (resource1) {
 		
-	System.out.println(Thread.currentThread().getName()+" have "+resource1);	
+	System.out.println(Thread.currentThread().getName()+" have "+resource1);
+	
+	resource1.notify();
 	}
 		
 		
@@ -53,7 +80,41 @@ public class Demo {
 	
 	thread1.start();
 	thread2.start();
-  	
+//  	
+//		PrintWriter r = new PrintWriter("abc.txt");
+//		
+//		r.write("helloj");
+//		r.write("helloj");
+//		r.write("helloj");
+//		r.write("helloj");
+//		
+//		r.flush();
+		
+//		List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+//		
+//		File file = new File("absh.txt");
+//		PrintStream pw = null;
+//		try {
+//			pw = new PrintStream(file);
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		}
+//		
+//		pw.println("Anuj 56.25 77.58");
+//		pw.println("Bharat 66.25 57.58");
+//		pw.println("Chaman 70.25 66.74");
+//		pw.println("Dhanush 58.25 95.74");
+//		pw.println("Garv 58.62 95.74");
+//		
+//		int count = list.stream().reduce(0, (e,ee) -> e+ee);
+//		
+//		System.out.println(count);
+		
+		
+		
+		
+		
+		
 		
 		
 		
